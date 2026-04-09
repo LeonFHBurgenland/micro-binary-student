a820fb94f5e5ac58bfb01b90abe76ef318dbeaa2
Leon <160027005+LeonFHBurgenland@users.noreply.github.com>
2026-04-09 10:23:36 +0200

    Send Hallo

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber)
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("HALLO")
+})
 radio.setGroup(191)
