e0e5338bfe931314ba990bb4b3857f1b847287e0
Leon <160027005+LeonFHBurgenland@users.noreply.github.com>
2026-04-09 11:25:56 +0200

    Send and recieve binary

--- main.ts
+++ main.ts
@@ -1,7 +1,48 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    inputMode = 0
+    recievedNum = receivedNumber
     basic.showNumber(receivedNumber)
 })
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("HALLO")
+    basic.clearScreen()
+    if (inputMode) {
+        binaryNumber = "" + binaryNumber + "0"
+        basic.clearScreen()
+        basic.showString("0")
+        basic.showString("_")
+    } else {
+        inputMode = 1
+        basic.clearScreen()
+        basic.showString("_")
+    }
 })
+function doSomething () {
+    while (false) {
+        basic.showString("")
+        basic.showString("_")
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.House)
+    radio.sendString(binaryNumber)
+    inputMode = 0
+    binaryNumber = ""
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    if (inputMode) {
+        binaryNumber = "" + binaryNumber + "1"
+        basic.clearScreen()
+        basic.showString("1")
+        basic.showString("_")
+    }
+})
+let binaryNumber = ""
+let inputMode = 0
+let recievedNum = 0
 radio.setGroup(191)
+basic.showString("STUDENT WAITING")
+while (!(recievedNum)) {
+    basic.showString("_")
+}
